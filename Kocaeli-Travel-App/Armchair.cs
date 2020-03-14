46bbd8b71999f0fa2ed06fa6eafeec056e0359ff
Onur Kantar <onurkantr@gmail.com>
2020-03-14 23:11:17 +0300

    Models

---
+++ Kocaeli-Travel-App/Armchair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kocaeli_Travel_App
+{
+    class Armchair
+    {
+        public string Id { get; set; }
+        public string Price { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ Kocaeli-Travel-App/Expedition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kocaeli_Travel_App
+{
+    class Expedition
+    {
+        public string Id { get; set; }
+        public string Road { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Capacity { get; set; }
+        public string Price { get; set; }
+        public string LicencePlate { get; set; }
+        public string Captain { get; set; }
+
+
+
+        //List Koltuklar
+    }
+}
